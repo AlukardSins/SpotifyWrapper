c41ec7a69d7ba309f65fcb3c2829f8dbde71e32e
Daniel Rodriguez <gameover_lifetime@outlook.com>
2020-08-06 00:41:15 -0500

    Second point done
    Waiting for feedback

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const cors = require('cors')
 
 const request = require('request')
 
+const Credentials = {
+  ClientId: '<YOUR CLIENT ID>',
+  ClientSecret: '<YOUR CLIENT SECRET>'
+}
 // To base64
-const encoded = Buffer.from('f4ff9ef1fb9a46e28540a65b47452b26:f3e982a07fbb4a2fa243a0dd588567f3').toString('base64')
+const encoded = Buffer.from(`${Credentials.ClientId}:${Credentials.ClientSecret}`).toString('base64')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -76,9 +80,12 @@ app.get('/v1/special-search', (req, response) => {
 ** optional:  valid_for defaukt = US
 ** output:    JSON response
 */
-app.post('/v1/artistInfo', (req, respose) => {
+app.post('/v1/artistInfo', (req, response) => {
   const ids = req.query.ids.length ? req.query.ids.split(',') : null
-  const orderBy = req.query.order_by || 'followers'
+  let orderBy = ''
+  if (req.query.order_by === 'followers' || req.query.order_by === 'popularity') {
+    orderBy = req.query.order_by || 'followers'
+  }
   const validFor = req.query.valid_for || 'US'
 
   if (ids && ids.length <= 5) {
@@ -97,14 +104,130 @@ app.post('/v1/artistInfo', (req, respose) => {
         // Proper https request
         if (!err && auth.statusCode === 200) {
           const authData = JSON.parse(authBody)
-          /*
-        request.get({
-          url: `${spotifyArtistAPI}artists?ids=${}`
 
-        }, (req, data, body) => {
+          let list = []
+          ids.forEach((id) => {
+            // Get artist info
+            const item = {}
+            request.get(
+              {
+                url: `${spotifyArtistAPI}artists/${id}`,
+                headers: {
+                  Authorization: `${authData.token_type} ${authData.access_token}`
+                }
+              },
+              (err, artist, artistBody) => {
+                if (!err && artist.statusCode === 200) {
+                  const artistData = JSON.parse(artistBody)
+                  item.artists = {
+                    spotify_url: artistData.external_urls.spotify,
+                    name: artistData.name,
+                    followers: artistData.followers.total,
+                    popularity: artistData.popularity,
+                    albums: [],
+                    related_artists: []
+                  }
 
-        })
-        */
+                  // Function to eliminate dupes goes here
+                  item.genres = artistData.genres
+
+                  // Get artist albums
+                  request.get(
+                    {
+                      url: `${spotifyArtistAPI}artists/${id}/albums?market=${validFor}&limit=3`,
+                      headers: {
+                        Authorization: `${authData.token_type} ${authData.access_token}`
+                      }
+                    },
+                    (err, albums, albumsBody) => {
+                      if (!err && albums.statusCode === 200) {
+                        const albumsData = JSON.parse(albumsBody)
+                        const albumsList = []
+
+                        albumsData.items.forEach((i) => {
+                          albumsList.push({
+                            name: i.name,
+                            release_date: i.release_date,
+                            type: i.type,
+                            is_available: true
+                          })
+                        })
+                        item.artists.albums = albumsList
+
+                        // Get artist albums alternate version
+                        /*
+                        request.get(
+                          {
+                            url: `${spotifyArtistAPI}artists/${id}/albums?limit=3`,
+                            headers: {
+                              Authorization: `${authData.token_type} ${authData.access_token}`
+                            }
+                          },
+                          (err, albums, albumsBody) => {
+                            if (!err && albums.statusCode === 200) {
+                              const albumsData = JSON.parse(albumsBody)
+                              const albumsList = []
+
+                              albumsData.items.forEach((i) => {
+                                albumsList.push({
+                                  name: i.name,
+                                  release_date: i.release_date,
+                                  type: i.type,
+                                  is_available: i.available_markets.includes(validFor)
+                                })
+                              })
+                              item.artists.albums = albumsList
+                            }
+                          }
+                        )
+                        */
+
+                        // Get artist related artists
+                        request.get(
+                          {
+                            url: `${spotifyArtistAPI}artists/${id}/related-artists`,
+                            headers: {
+                              Authorization: `${authData.token_type} ${authData.access_token}`
+                            }
+                          },
+                          (err, related, relatedBody) => {
+                            if (!err && related.statusCode === 200) {
+                              let relatedData = JSON.parse(relatedBody)
+                              const relatedList = []
+                              relatedData = relatedData.artists.splice(0, 3)
+                              relatedData.forEach((i) => {
+                                relatedList.push({
+                                  name: i.name,
+                                  followers: i.followers.total,
+                                  popularity: i.popularity
+                                })
+                              })
+                              item.artists.related_artists = relatedList
+                              list.push(item)
+
+                              if (list.length === ids.length) {
+                                if (orderBy === 'followers') {
+                                  list.sort((x, y) => {
+                                    return y.artists.followers - x.artists.followers
+                                  })
+                                } else if (orderBy === 'popularity') {
+                                  list.sort((x, y) => {
+                                    return y.artists.popularity - x.artists.popularity
+                                  })
+                                }
+
+                                return response.send(list)
+                              }
+                            }
+                          }
+                        )
+                      }
+                    }
+                  )
+                }
+              }
+            )
+          })
         }
       }
     )
